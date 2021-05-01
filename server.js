5ac862012914d0a2850f5c3e8ddb6ed615249f85
maulikdadhaniya <maulik.dadhaniya@live.com>
2021-05-01 14:19:19 +0530

    change cluster

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.MONGODB_URI|| 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
