ba631771faf201f898bb68417d11e572642966f1
maulikdadhaniya <maulik.dadhaniya@live.com>
2021-05-01 14:46:44 +0530

    change cluster

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://cluster0-shard-00-00.tngpx.mongodb.net:27017"
+  origin: process.env.MONGODB_URI
 };
 
 app.use(cors(corsOptions));
